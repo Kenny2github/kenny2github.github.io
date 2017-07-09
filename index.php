9aa466d60b0354dff8e9201bb52764886d98b33b
Ken <kenny2minecraft@gmail.com>
2017-07-09 10:53:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,72 +1,70 @@
-<?php echo <<<XML
-<!doctype html>
 <html>
 <head>
-    <title>Kenny2github</title>
-    <link rel="stylesheet" href="main.css"/>
-    <script src="start.js"></script>
+	<title>Kenny2github</title>
+	<link rel="stylesheet" href="main.css"/>
+	<script src="start.js"></script>
 </head>
 <body>
     <div id="header"><a href="#Me">Me in a nutshell</a><a href="#Random">Random stuff about me</a><a href="#Contact">Contact Info</a><a href="#Do">What I do</a><a href="#Done">What I've done</a><a href="#Programming">Me programming-wise</a><a href="#Quotes">Favorite Quotes</a></div>
-    <h1 id="Me">Me in a nutshell</h1>
-    <p>I am a monster who hides in a cave who recently decided to try to see what Github was like. In Github, I (so far) have reported quite a few issues, and thats... basically... it.</p>
-    <p>I'm an American citizen born and raised in Hong Kong. I live in Hong Kong and it's a lot of fun without guns or sales tax</p>
-    <h1 id="Random">Random stuff about me</h1>
-    <ul>
-        <li>I'm good at choral singing</li>
-        <li>I live in Hong Kong; my timezone is GMT+8:00</li>
-        <li>I'm colorblind<sup class="note">[note]<div>I am a strong deutan; I have difficulty telling the difference between green/red and blue/purple.</div></sup></li>
-        <li>I like reading legalese</li>
-        <li>I'm in <span style="background-color:blue">Ravenclaw</span></li>
-        <li>I speak:
-            <ul>
-                <li>English</li>
-                <li>Cantonese and Mandarin</li>
-                <li>Morse code</li>
-                <li>German numbers</li>
-                <li>Learning Vietnamese</li>
-            </ul>
-        </li>
-        <li>I can read:
-            <ul>
-                <li>English</li>
-                <li>Traditional Chinese</li>
-                <li>Morse code</li>
-                <li>Chicken scratch</li>
-            </ul>
-        </li>
-        <li>My favorite things:
-            <ul>
+	<h1 id="Me">Me in a nutshell</h1>
+	<p>I am a monster who hides in a cave who recently decided to try to see what Github was like. In Github, I (so far) have reported quite a few issues, and thats... basically... it.</p>
+	<p>I'm an American citizen born and raised in Hong Kong. I live in Hong Kong and it's a lot of fun without guns or sales tax</p>
+	<h1 id="Random">Random stuff about me</h1>
+	<ul>
+	    <li>I'm good at choral singing</li>
+	    <li>I live in Hong Kong; my timezone is GMT+8:00</li>
+	    <li>I'm colorblind<sup class="note">[note]<div>I am a strong deutan; I have difficulty telling the difference between green/red and blue/purple.</div></sup></li>
+	    <li>I like reading legalese</li>
+	    <li>I'm in <span style="background-color:blue">Ravenclaw</span></li>
+	    <li>I speak:
+		<ul>
+		    <li>English</li>
+		    <li>Cantonese and Mandarin</li>
+		    <li>Morse code</li>
+		    <li>German numbers</li>
+		    <li>Learning Vietnamese</li>
+		</ul>
+	    </li>
+	    <li>I can read:
+		<ul>
+		    <li>English</li>
+		    <li>Traditional Chinese</li>
+		    <li>Morse code</li>
+		    <li>Chicken scratch</li>
+		</ul>
+	    </li>
+	    <li>My favorite things:
+		<ul>
                 <li>Color: Blue</li>
-                <li>Animal: Cheetah</li>
-                <li>Game: Minecraft/Undertale</li>
-                <li>Person: <span style="color:gray">[redacted]</span></li>
-                <li>Font: Courier New (in case you didn't notice the website)</li>
-                <li>OS: Windows</li>
-                <li>Programming language: Python</li>
-            </ul>
-        </li>
-        <li>I play:
-            <ul>
-                <li>Undertale: All routes except debug and hacker completed</li>
-                <li>Minecraft: I specialize in redstone, never played survival</li>
-                <li>Clash Royale: <a href="http://statsroyale.com/profile/GCY2C02Y">stats</a>, favorite deck: Valkyrie, Hog Rider, Witch, Musketeer, Mirror, Zap, Fireball, Minion Horde</li>
-                <li>King of Thieves: Name: Demosthenes, League: Orbeuseus, Rating: 1239953<sup class="note">[note]<div>As of 6/6/2017 02:17 (UTC)</div></sup>, Guild: GemRobbers, highest gem tier: 300K+ (actual value >800K)</li>
-            </ul>
-        </li>
-        <li>If you somehow show up where I live and identify yourself as someone I know of, I will make you spaghetti.</li>
-        <li>I'm picky about my socks</li>
-    </ul>
-    <h1 id="Contact">Contact Info</h1>
-    <p>Email: <a href="mailto:kenny2minecraft@gmail.com">kenny2minecraft at gmail dot com</a></p>
-    <h1 id="Do">What I do</h1>
-    <ul><!-- I just love lists :D -->
-        <li>Experiment with new ways of putting programming techniques together</li>
-        <li>Learn bits and bobs about new programming languages (such as WScript... ugh)</li>
-        <li>Deal with life</li>
-        <li>Am happy with someone ♥</li>
-    </ul>
-    <h1 id="Done">What I've done</h1>
+		    <li>Animal: Cheetah</li>
+		    <li>Game: Minecraft/Undertale</li>
+		    <li>Person: <span style="color:gray">[redacted]</span></li>
+		    <li>Font: Courier New (in case you didn't notice the website)</li>
+		    <li>OS: Windows</li>
+		    <li>Programming language: Python</li>
+		</ul>
+	    </li>
+	    <li>I play:
+		<ul>
+		    <li>Undertale: All routes except debug and hacker completed</li>
+		    <li>Minecraft: I specialize in redstone, never played survival</li>
+		    <li>Clash Royale: <a href="http://statsroyale.com/profile/GCY2C02Y">stats</a>, favorite deck: Valkyrie, Hog Rider, Witch, Musketeer, Mirror, Zap, Fireball, Minion Horde</li>
+		    <li>King of Thieves: Name: Demosthenes, League: Orbeuseus, Rating: 1239953<sup class="note">[note]<div>As of 6/6/2017 02:17 (UTC)</div></sup>, Guild: GemRobbers, highest gem tier: 300K+ (actual value >800K)</li>
+		</ul>
+	    </li>
+	    <li>If you somehow show up where I live and identify yourself as someone I know of, I will make you spaghetti.</li>
+	    <li>I'm picky about my socks</li>
+	</ul>
+	<h1 id="Contact">Contact Info</h1>
+	<p>Email: <a href="mailto:kenny2minecraft@gmail.com">kenny2minecraft at gmail dot com</a></p>
+	<h1 id="Do">What I do</h1>
+	<ul><!-- I just love lists :D -->
+	    <li>Experiment with new ways of putting programming techniques together</li>
+	    <li>Learn bits and bobs about new programming languages (such as WScript... ugh)</li>
+	    <li>Deal with life</li>
+	    <li>Am happy with someone ♥</li>
+	</ul>
+	<h1 id="Done">What I've done</h1>
 	<ul>
 		<li>Won Merit (and as a prize, a RPi 3 Model B) in the <a href="https://cschallenge.cs.cityu.edu.hk/2016-results/">CS Challenge 2016</a> Secondary School category</li>
 		<li>Won Merit (and as a prize, a USB battery pack) in the <a href="https://cschallenge.cs.cityu.edu.hk/results/">CS Challenge 2017</a> Secondary School category</li>
@@ -83,8 +81,8 @@
 	<blockquote>
 		<p>No one in the world can change Truth. What we can do and should do is to seek truth and to serve it when we have found it. The real conflict is the inner conflict. Beyond armies of occupation and the hecatombs of extermination camps, there are two irreconcilable enemies in the depth of every soul: good and evil, sin and love. And what use are the victories on the battlefield if we ourselves are defeated in our innermost personal selves?</p>
 		<p style="float:right;font-size:70%">-Saint Maximilian Kolbe</p></blockquote><sup class="note">[note]<div>I'm not religious; Saint Maximilian Kolbe is the patron saint of HAM radio operators.</div></sup>
-    <blockquote><p>Everything is better when you add a little cheddar, because when you have cheese you're life's at ease.</p><p style="float:right;font-size:70%">cheddargirl</p></blockquote>
+	<blockquote><p>Everything is better when you add a little cheddar, because when you have cheese you're life's at ease.</p><p style="float:right;font-size:70%">cheddargirl</p></blockquote>
+	<p><?php echo "Wonder if this works?"; ?></p>
 	<script src="end.js"></script>
 </body>
 </html>
-XML; ?>
