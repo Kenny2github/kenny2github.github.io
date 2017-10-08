e2d61a1d6fff4df71e28e6b1c2ef3dd8959f822a
Ken <kenny2minecraft@gmail.com>
2017-10-08 12:30:50 +0800

    update to work with both scratch-www and scratchr2

--- index/discuss.user.js
+++ index/discuss.user.js
@@ -2,8 +2,13 @@
 // @name           Bring it Back
 // @description    Bring back the Discuss button in the header!
 // @match          https://scratch.mit.edu/*
+// @run-at         document-end
 // ==/UserScript==
 
+window.addEventListener('load', function(){
 link = document.createElement('li');
 link.innerHTML = '<a href="/discuss">Discuss</a>';
-document.querySelector('div#topnav ul.site-nav').insertBefore(link, document.querySelector('div#topnav ul.site-nav').getElementsByTagName('li')[2]);
+link.className = 'link discuss';
+try {document.querySelector('div#navigation div.inner ul').insertBefore(link, document.querySelector('div#navigation div.inner ul').getElementsByTagName('li')[3]);}
+catch(err) {document.querySelector('div#topnav ul.site-nav').insertBefore(link, document.querySelector('div#topnav ul.site-nav').getElementsByTagName('li')[2]);}
+}, false);
